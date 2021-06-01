3d4aec6e9ae17fec215cb8fcaa9558818ebd1a0b
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-06-01 11:28:53 +0530

    Update android.py

--- plugins/android.py
+++ plugins/android.py
@@ -9,7 +9,12 @@
 from bs4 import BeautifulSoup
 from requests import get
 from userge import Message, userge
+from aiohttp import ClientSession
+import ujson
+from typing import Optional
+import asyncio
 
+magisk_release = "https://raw.githubusercontent.com/topjohnwu/magisk-files/master/{}.json"
 
 @userge.on_cmd(
     "twrp",
@@ -18,7 +23,6 @@ from userge import Message, userge
 )
 async def device_recovery(message: Message):
     """Get Latest TWRP"""
-    message.reply_to_message
     args = message.filtered_input_str
     if args:
         device = args
@@ -44,29 +48,23 @@ async def device_recovery(message: Message):
     await message.edit(reply)
 
 
+
+
+
 @userge.on_cmd("magisk$", about={"header": "Get Latest Magisk Zip and Manager"})
 async def magisk_(message: Message):
     """Get Latest MAGISK"""
-    magisk_repo = "https://raw.githubusercontent.com/topjohnwu/magisk_files/"
-    magisk_dict = {
-        "⦁ 𝗦𝘁𝗮𝗯𝗹𝗲": magisk_repo + "master/stable.json",
-        "⦁ 𝗕𝗲𝘁𝗮": magisk_repo + "master/beta.json",
-        "⦁ 𝗖𝗮𝗻𝗮𝗿𝘆": magisk_repo + "canary/canary.json",
-    }
-    releases = "<code><i>𝗟𝗮𝘁𝗲𝘀𝘁 𝗠𝗮𝗴𝗶𝘀𝗸 𝗥𝗲𝗹𝗲𝗮𝘀𝗲:</i></code>\n\n"
-    for name, release_url in magisk_dict.items():
-        data = get(release_url).json()
-        if "canary" in release_url:
-            data["app"]["link"] = magisk_repo + "canary/" + data["app"]["link"]
-            data["magisk"]["link"] = magisk_repo + "canary/" + data["magisk"]["link"]
-            data["uninstaller"]["link"] = (
-                magisk_repo + "canary/" + data["uninstaller"]["link"]
-            )
-
-        releases += (
-            f'{name}: [ZIP v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
-            f'[APK v{data["app"]["version"]}]({data["app"]["link"]}) | '
-            f'[Uninstaller]({data["uninstaller"]["link"]})\n'
-        )
-
-    await message.edit(releases, disable_web_page_preview=True)
+    async with ClientSession() as session:
+        async def format_release(x: str) -> Optional[str]:
+            async with session.get(magisk_release.format(x)) as resp:
+                if resp.status != 200:
+                    return
+               r_data = ujson.loads(await resp.text())
+            out = f"⦁ <b>{x.title()}</b>:  <a href={r_data.get('link')}>v{r_data.get('version')}</a>"
+            if changelog := r_data.get("note"):
+                out += f"  |  [Changelog]({changelog})"
+            return out
+        
+        releases = await asyncio.gather(*map(format_release, ["stable", "beta", "canary"]))
+    
+    await message.edit("𝗟𝗮𝘁𝗲𝘀𝘁 𝗠𝗮𝗴𝗶𝘀𝗸 𝗥𝗲𝗹𝗲𝗮𝘀𝗲:\n" + "\n".join(list(filter(None, releases))), disable_web_page_preview=True)
